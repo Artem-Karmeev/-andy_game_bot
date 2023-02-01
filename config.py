5b4c53f1839d2c672a1a6bcadecfaa474b8139c8
Artem Karmeev <skinnynew161@gmail.com>
2023-02-01 22:45:16 +0300

    крайний

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 from aiogram import Dispatcher,Bot
 
-bot = Bot(token='')
+bot = Bot(token='5977832137:AAH8ZcCMpU0t5ZmZxc03S_jZsvKiH8d9i00')
 dp = Dispatcher(bot) 
 
--- handler.py
+++ handler.py
@@ -1,13 +1,12 @@
 from aiogram import types
 from aiogram.dispatcher.filters import Text
-from random import randint as rnt
+from random import randint as RI
 from config import dp
 from my_bot import my_bot
 
 from text import text_1
 
-count_candy = 0
-fate = 0
+users = {}
 
 
 @dp.message_handler(commands=['start'])
@@ -18,45 +17,54 @@ async def start(message: types.Message):
 @dp.message_handler(commands=['help'])
 async def start(message: types.Message):
     await message.answer(text_1)
+    await message.answer(f'УТЧ: Чтобы задать количество конфет в ведите /game "NUM", где NUM это желаемое кол-во 🍬 ')
 
 
 @dp.message_handler(commands=['game'])
 async def start(message: types.Message):
-    global count_candy
-    global fate
-    fate = rnt(0,1)
-    count_candy = 150
-    await message.answer(f'\nДа начнутся голодные игры!\n На столе лежит 150 конфет')
+
+    global users
+    candys = 150
+
+    if len(message.text) != 5 and message.text[5:].replace(' ','').isdigit():
+        candys = int(message.text[5:].replace(' ',''))
+
+    users[message.from_user.id] = [candys]
+    await message.answer(f'\nДа начнутся голодные игры!\n На столе лежит {users[message.from_user.id][0]} 🍬🍬🍬')
+
+    fate = RI(0,1)
+
     if fate:
         await message.answer(f'По итогам жеребьевки, первый ход за тобой 🫵, сколько возьмешь 🍬? ')
     else:
         await message.answer(f'По итогам жеребьевки, первым ходит бот')
-        num = my_bot(count_candy)
-        count_candy -= num
-        await message.answer(f'Бот взял {num} 🍬, на столе осталось {count_candy} 🍬.')
+        num = my_bot(users[message.from_user.id][0])
+        users[message.from_user.id][0] -= num
+        await message.answer(f'Бот взял {num} 🍬, на столе осталось {users[message.from_user.id][0]} 🍬.')
 
 
 @dp.message_handler()
 async def mes_all(message: types.Message):
-    global count_candy
-    global fate
+    global users
 
-    if count_candy > 0:
 
+    if message.from_user.id in users:
         if message.text.isdigit(): 
             if 0 < int(message.text) < 29:
-                count_candy -= int(message.text)
-                if count_candy > 0:
-                    await message.answer(f'На столе осталось {count_candy} 🍬')
-                    num = my_bot(count_candy)
-                    count_candy -= num
-                    if count_candy > 0:
-                        await message.answer(f'Бот взял {num} 🍬, на столе осталось {count_candy} 🍬. \nСколько возьмешь конфет?')
+                users[message.from_user.id][0] -= int(message.text)
+                if users[message.from_user.id][0] > 0:
+                    await message.answer(f'На столе осталось {users[message.from_user.id][0]} 🍬')
+                    num = my_bot(users[message.from_user.id][0])
+                    users[message.from_user.id][0] -= num
+                    if users[message.from_user.id][0] > 0:
+                        await message.answer(f'Бот взял {num} 🍬, на столе осталось {users[message.from_user.id][0]} 🍬. \nСколько возьмешь 🍬?')
                     else:
 
                         await message.answer('... и их забрал бот! \nПобедил бот 🤖, грядет восстание машин!')
+                        
                 else:
                     await message.answer(f'{message.from_user.first_name} одерживает победу🏆!')
+                    users.pop(message.from_user.id)
             else:
                 await message.answer('За раз можно взять не более 28 конфет!')
         else:
